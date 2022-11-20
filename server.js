d50fd94291dba79475a7290a60919cc245c6b074
muhafash <me.muhfarash@gmail.com>
2022-11-20 16:45:23 +0700

    Baru

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express= require('express');
 const app = express();
 
-var server = app.listen(3000, ((listening)=>{console.log("Listening.....")}))
+// var server = app.listen(3000, ((listening)=>{console.log("Listening.....")}))
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.get("/", ((req, res)=>{
     res.send("home")
