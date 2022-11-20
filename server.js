12dd03344c71d297e71d84dbabcf71ee0589bee3
muhafash <me.muhfarash@gmail.com>
2022-11-20 16:24:26 +0700

    baru masuk

---
+++ server.js
@@ -0,0 +1,23 @@
+// Deklarasi env
+const express= require('express');
+const app = express();
+
+var server = app.listen(3000, ((listening)=>{console.log("Listening.....")}))
+
+app.use(express.static("public"));
+
+app.get("/", ((req, res)=>{
+    res.send("home")
+}));
+
+app.get("/search", ((req, res)=>{
+    res.send("search")
+}));
+
+app.get("/search/:sub", ((req, res)=>{
+    res.send("search sub" + req.params.sub)
+}));
+
+app.get("/search/:sub/:id", ((req, res)=>{
+    res.send("search sub" + req.params.id);
+}));
